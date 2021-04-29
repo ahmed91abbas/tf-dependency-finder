dcd3ebfc77d6d045df8b03c5927065acc72f5169
Ahmed Abbas <ahmed.abbas1@ingka.ikea.com>
2021-04-29 15:04:58 +0200

    create tf dep finder action

--- terraform/module1/main.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "module2" {
-  backend = "gcs"
-
-  config = {
-    bucket = var.project
-    prefix = join("/", compact(["terraform-state", "module2", var.seed]))
-  }
-}
--- terraform/module2/main.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "module3" {
-  backend = "gcs"
-
-  config = {
-    bucket = var.project
-    prefix = join("/", compact(["terraform-state", "module3", var.seed]))
-  }
-}
